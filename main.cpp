d6c7fafcdf6451dbc94e296cf061bae37a99aba9
maxifom <main@sites4dudes.ru>
2018-11-20 13:55:31 +0500

    Super unworking solution
    Super unworking solution

--- main.cpp
+++ main.cpp
@@ -11,65 +11,78 @@ using namespace std;
 int main()
 {
 	//Спрашиваем у пользователя и считываем размер матрицы, с которой будем работать
-    printf("Enter Size of Matrix: ");
-    int N;
-    scanf("%i", &N);
+	printf("Enter Size of Matrix: ");
+	int N;
+	scanf("%i", &N);
 	//Создаем матрицу заданного размера
-    Matr A = InitMatr(N, N);
+	Matr A = InitMatr(N, N);
+	//SquareMatr A2 = InitMatr(N, N);
 	//двва способа заполнить матрицу, раскоменнтировать тот который нужен 
 	//заполняем матрицу случайными числами ( для больших размеров матриц актуально)
-    A = EnterRandom(A);
+	A = EnterRandom(A);
 	//заполняем матрицу с клавиатуры, актуально для небольши размеров матрицы
-    //A = EnterMatr(A);
-    //выводим матрицу на экран
+	//A = EnterMatr(A);
+	//A2 = EnterMatr(A2);
+	//Print(A*A2);
+	//std::cout << Spur(A*A2) << std::endl;
+	//выводим матрицу на экран
 	Print(A);
 	//создаем две вспомогательных матрицы, они нужны для расчетов...
-    Matr A1 = InitMatr(N, N);
-    Matr B1 = InitMatr(N, N);
+	Matr A1 = InitMatr(N, N);
+	Matr B1 = InitMatr(N, N);
 	//Матрица B1 сначала единичная
-    B1 = EnterUnit(B1);
+	B1 = EnterUnit(B1);
+	//std::cout << "Unit Matrix:";
+	//Print(B1 * B1);
+	//Print(A*B1);
 	//создаем матрицу, где будет записана обратная
-    Matr UA = InitMatr(N, N);
+	Matr UA = InitMatr(N, N);
 	//создаем единичную матрицу, нужна для расчетов
-    Matr E = InitMatr(N, N);
-    E = EnterUnit(E);
+	Matr E = InitMatr(N, N);
+	E = EnterUnit(E);
+	//Print(A-A);
 	//создаем массив, где бдут записаны коэффициенты характеристического полинома
-    double* lamb = new double[N];
+	double* lamb = new double[N];
 	//Перед началом работы алгоритма снимаем время, чтобы оценить время выполнения расчетов
-    unsigned int start_time =  clock();
+	unsigned int start_time = clock();
 	//начинаем сам алгоритм Леверье-Фаддеева, запускаем не до последнего элемента
 	//последний шаг считаем отдельно
-    for (int i = 1; i < N; i++)
-    {
+	for (int i = 1; i < N; i++)
+	{
 		//Пересчитываем матрицу A1 и B1 по формулам Фаддеева
-        A1 = A * B1;
+		A1 = A * B1;
+		//std::cout << "A1:" << std::endl;
+		//Print(A1);
+		//std::cout << std::endl;
 		//Записываем очередной коэффициент характеристического полинома
-        lamb[i - 1] = Spur(A1) / i;
-        B1 = A1 - lamb[i - 1] * E;
-    }
+		lamb[i - 1] = Spur(A1) / i;
+		B1 = A1 - lamb[i - 1] * E;
+	}
 	//На последнем шаге считаем только матрицу A1 икоэффициент
 	//матрица B1 на посчледнем шаге должна уйти в ноль
-    A1 = A * B1;
+	A1 = A * B1;
 	//записали последний коэффициент полинома
-    lamb[N - 1] = Spur(A1) / N ;
+	lamb[N - 1] = Spur(A1) / N;
 	//посчитали обратную матрицу, она считается через предпоследнюю матрицу B1
 	//Почему мы и не считаем B 1еще раз...
-    UA = (1 / lamb[N - 1]) * B1;
+	UA = (1 / lamb[N - 1]) * B1;
 	//Смотрим время в конце работ алгоритма...
-    unsigned int end_time = clock();
+	unsigned int end_time = clock();
 	//Выводим коэффициенты характеристического полинома
-    printf("\n\nCoeff polynom's: \n");
-    for(int i = 0; i < N; i++)
-        printf("Lambda %i  --   %6.8f\n", i + 1, lamb[i]);
+	printf("\n\nCoeff polynom's: \n");
+	for (int i = 0; i < N; i++)
+		printf("Lambda %i  --   %6.8f\n", i + 1, lamb[i]);
 	//Печатаем обратную матрицу
-    printf("\n\nInverse Matrix:");
-    Print(UA);
+	printf("\n\nInverse Matrix:");
+	Print(UA);
 	//Проверяем правильность обратной матрицы, выводм результат умножения обратной амтрицы и исходной
-    printf("\n\nCheck to Inverse matrix: A^-1 * A");
-    Print(UA * A);
+	printf("\n\nCheck to Inverse matrix: A^-1 * A");
+	//printf("Print A");
+	//Print(A);
+	Print(UA * A);
 	//Выводим на экран время работы алгоритма...
-    printf("Run time --- %i\n\n", end_time - start_time);
+	printf("Run time --- %i\n\n", end_time - start_time);
 
 
-    return 0;
+	return 0;
 }
--- matrix.h
+++ matrix.h
@@ -2,11 +2,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <time.h>
 
-struct Matr // Новый тип Матрица
+struct Matr // Новый тип Квадратная матрица
 {
-	double** M; // Собственно сама матрица
-	int row;    // количество строк
-	int col;    // количество столбцов
+	double* M; // Собственно сама матрица
+	int order;  //Порядок матрицы
+	int size;	//Размер матрицы
 };
 
 // Функция определения матрицы. по ее размерам. Возвращает пустую матрицу
@@ -15,15 +15,10 @@ Matr InitMatr(int n, int m)
 	//Определяем временную матрицу
 	Matr temp;
 	//выделяем память под строки
-	temp.M = new double*[n];
+	temp.size = n * n;
+	temp.M = new double[temp.size];
 	//запсываем размеры матрицы
-	temp.row = n;
-	temp.col = m;
-	//выделяем память под каждый из столбцов...
-	for (int i = 0; i < temp.row; i++)
-	{
-		temp.M[i] = new double[m];
-	}
+	temp.order = n;
 	//возвращаем созданную матрицу
 	return temp;
 }
@@ -32,10 +27,12 @@ Matr InitMatr(int n, int m)
 void Print(Matr A)
 {
 	printf("\n\nEnter Matrix To Console!!!\n");
-	for (int i = 0; i < A.row; i++)
+	for (int i = 0; i < A.order; i++)
 	{
-		for (int j = 0; j < A.col; j++)
-			printf("%10.8f    ", A.M[i][j]);
+		for (int j = 0; j < A.order; j++)
+		{
+			printf("%10.8f    ", A.M[i*A.order + j]);
+		}
 		printf("\n");
 	}
 }
@@ -43,10 +40,9 @@ void Print(Matr A)
 // Заполнение матрицы нолями, все элементы матрицы нули
 Matr EnterZero(Matr A)
 {
-	for (int i = 0; i < A.row; i++)
+	for (int i = 0; i < A.size; i++)
 	{
-		for (int j = 0; j < A.col; j++)
-			A.M[i][j] = 0;
+		A.M[i] = 0;
 	}
 	return A;
 }
@@ -55,10 +51,9 @@ Matr EnterZero(Matr A)
 Matr EnterRandom(Matr A)
 {
 	srand((unsigned int)time(0));
-	for (int i = 0; i < A.row; i++)
+	for (int i = 0; i < A.size; i++)
 	{
-		for (int j = 0; j < A.col; j++)
-			A.M[i][j] = (double)rand()/(double)RAND_MAX;
+		A.M[i] = (double)rand() / (double)RAND_MAX;
 	}
 	return A;
 }
@@ -66,28 +61,27 @@ Matr EnterRandom(Matr A)
 // заполняет матрицу числами введенными с клавиатуры
 Matr EnterMatr(Matr A)
 {
-    printf("\nEnter elements of matrix from Console...");
-	for (int i = 0; i < A.row; i++)
+	printf("\nEnter elements of matrix from Console...");
+
+	for (int i = 0; i < A.order; i++)
 	{
-		for (int j = 0; j < A.col; j++)
-        {
-            printf("\nEnter A[%i][%i]: ", i, j);
-            double temp;
-            scanf("%lf", &temp);
-            A.M[i][j] = temp;
-        }
+		for (int j = 0; j < A.order; j++)
+		{
+			printf("\nEnter A[%i][%i]: ", i, j);
+			scanf("%lf", &A.M[i*A.order + j]);
+		}
 	}
+
 	return A;
 }
 
 // Создание единичной матрицы
 Matr EnterUnit(Matr A)
 {
-	for (int i = 0; i < A.row; i++)
+	A = EnterZero(A);
+	for (int i = 0; i < A.size; i += A.order + 1)
 	{
-		for (int j = 0; j < A.col; j++)
-			A.M[i][j] = 0;
-		A.M[i][i] = 1;
+		A.M[i] = 1;
 	}
 	return A;
 }
@@ -95,20 +89,16 @@ Matr EnterUnit(Matr A)
 // Возвращает разность двух матриц
 Matr operator -(Matr A, Matr B)
 {
-	for (int i = 0; i < A.row; i++)
-		for (int j = 0; j < A.col; j++)
-			A.M[i][j] -= B.M[i][j];
-
+	for (int i = 0; i < A.size; i++)
+		A.M[i] -= B.M[i];
 	return A;
 }
 
 // Возвращает сумму двух матриц
 Matr operator +(Matr A, Matr B)
 {
-	for (int i = 0; i < A.row; i++)
-		for (int j = 0; j < A.col; j++)
-			A.M[i][j] += B.M[i][j];
-
+	for (int i = 0; i < A.size; i++)
+		A.M[i] += B.M[i];
 	return A;
 }
 
@@ -117,13 +107,13 @@ Matr operator *(Matr A, Matr B)
 {
 	//создаем вспомогательную матрицу
 	Matr C;
-	C = InitMatr(A.row, B.col);
+	C = InitMatr(A.order, B.order);
 	C = EnterZero(C);
 	//во вспомогательную матрицу записываем результат умножения двух заданных матриц
-	for (int i = 0; i < A.row; i++)
-    	for (int j = 0; j < B.col; j++)
-			for (int l = 0; l < A.col; l++ )
-			C.M[i][j] += A.M[i][l] * B.M[l][j];
+	for (int i = 0; i < A.order; i++)
+		for (int j = 0; j < B.order; j++)
+			for (int l = 0; l < A.order; l++)
+				C.M[i*A.order + j] += A.M[i*A.order + l] * B.M[l*A.order + j];
 
 	//возвращаем результат умножения
 	return C;
@@ -132,24 +122,18 @@ Matr operator *(Matr A, Matr B)
 // Умножение матрицы на скаляр
 Matr operator * (double a, Matr B)
 {
-	//создаем вспомогательную матрицу
-	Matr C;
-	C = InitMatr(B.row, B.col);
-	C = EnterZero(C);
 	//во вспомогательную матрицу записываем результат умножения матрицы на скаляр
-	for (int i = 0; i < B.row; i++)
-    	for (int j = 0; j < B.col; j++)
-			C.M[i][j] = B.M[i][j] * a;
-
-	return C;
+	for (int i = 0; i < B.size; i++)
+		B.M[i] *= a;
+	return B;
 }
 
 
 //Функция для рассчета следа матрицы
 double Spur(Matr A)
 {
-    double t = 0;
-    for (int i = 0; i < A.row; i++)
-        t += A.M[i][i];
-    return t;
+	double t = 0;
+	for (int i = 0; i < A.size; i += A.order + 1)
+		t += A.M[i];
+	return t;
 }
